e5a3116e545b61420dccd6ec74f94aa6dbbf8cc3
Aarna Singh <114718233+aarnasingh812@users.noreply.github.com>
2024-08-26 17:22:02 +0530

    Update predict_pipeline.py

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -8,8 +8,8 @@ class PredictPipeline:
         pass
     def predict(self,features):
         try:
-           model_path='artifacts\model.pkl'
-           preprocessor_path='artifacts\preprocessor.pkl'
+           model_path='artifacts/model.pkl'
+           preprocessor_path='artifacts/preprocessor.pkl'
            model=load_object(file_path=model_path)
            preprocessor=load_object(file_path=preprocessor_path)
            data_scaled=preprocessor.transform(features)
